80f48abf01abd706b85e9ce263cc9a7d4b43ebf1
MJ-SEO <21600343@handong.edu>
2022-01-21 10:51:32 +0900

    Identidock test

--- identidock/app/tests.py
+++ identidock/app/tests.py
@@ -1,7 +1,6 @@
 import unittest
 import identidock
 
-
 class TestCase(unittest.TestCase):
 
 	def setUp(self):
@@ -15,7 +14,7 @@ class TestCase(unittest.TestCase):
 		assert 'Moby Dock' in str(page.data)
 
 	def test_html_escaping(self):
-		age = self.app.post("/", data=dict(name='"><b>TEST</b><!--'))
+		page = self.app.post("/", data=dict(name='"><b>TEST</b><!--'))
 		assert '<b>' not in str(page.data)
 
 if __name__ == '__main__':
